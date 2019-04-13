92bbe722e8f5cbe50483c5336fbd2dcb757e9123
xuexiaoping <506169401@qq.com>
2019-04-13 11:19:33 +0800

    xue-完成

--- js/bijia.js
+++ js/bijia.js
@@ -1,53 +1,57 @@
-/*
- * @Author: zhengwei
- * @Date:   2016-11-23 23:23:00
- * @Last Modified by:   zhengwei
- * @Last Modified time: 2016-11-27 10:27:17
- */
+$(function(){
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
 
-'use strict';
-$(function() {
-    var productid = getQueryString("productid");
-    console.log(productid);
-    getProduct(productid)
+    var productid= getParameter(location.search).productid  || 1;
+    var category = getParameter(decodeURI(location.search)).category 
 
-    function getProduct(productid) {
-        $.ajax({
-            url: "http://193.112.55.79:9090/api/getproduct",
-            data: {
-                "productid": productid
-            },
-            success: function(data) {
-                console.log(data);
-                var html = template("productInfoTmp", data);
-                $('.product-info').html(html);
-                //获取评论的时候等商品先出来了再获取评论 
-                getProductCom(productid);
-            }
-        })
-    }
-
-    function getProductCom(productid) {
-        $.ajax({
-            url: "http://193.112.55.79:9090/api/getproductcom",
-            data: {
-                "productid": productid
-            },
-            success: function(data) {
-                // console.log(data);
-                var html = template("productComTmp", data);
-                console.log($('.product-com-list'));
-                $('.product-com-list').html(html);
-            }
-        })
-    }
-    //是用来获取url中的参数的值的 根据参数名获取参数值
-    function getQueryString(name) {
-        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null) {
-            return unescape(r[2]);
+    // console.log(productid);
+    $.ajax({
+        url: 'http://193.112.55.79:9090/api/getproduct',
+        data: { "productid": productid},
+        dataType:'json',
+        success:function(result){
+            result.result.category = category;
+                console.log(result);
+            var html = template('getproductTemp',result)      
+            $('.product_bijia').html(html)
+            var navhtml = template('navTemp', result) 
+            $('.nav').html(navhtml)
         }
-        return null;
+    })
+
+
+
+
+    $.ajax({
+        url: 'http://193.112.55.79:9090/api/getproductcom',
+        data: { "productid": productid },
+        dataType: 'json',
+        success: function (result) {
+            console.log(result);
+            var html = template('comTemp', result)
+            $('.product_com_list').html(html)
+        }
+    })
+
+
+    // 获取url地址栏的后面id函数
+    function getParameter(url) {
+        var obj = {}
+        // location.search:url中?及?后面的内容
+        url = url.substring(1) //cid=5&name=jack
+        // 先按&拆分
+        var arr = url.split('&') //['cid=5','name=jack']
+        // 遍历进行第二次拆分
+        for (var i = 0; i < arr.length; i++) {
+            var temp = arr[i].split('=') //['cid',5]
+            obj[temp[0]] = temp[1] // obj['cid'] = 5
+        }
+        return obj
     }
-});
+    getParameter(location.search)
+    // decodeURI 当地址栏有中文的时候用
+    // console.log(getParameter(decodeURI(location.search)).category);
+
+})
--- js/productlist.js
+++ js/productlist.js
@@ -1,87 +1,139 @@
-/*
- * @Author: zhengwei
- * @Date:   2016-11-23 23:23:00
- * @Last Modified by:   zhengwei
- * @Last Modified time: 2016-11-27 09:39:08
- */
+$(function(){
+    $('.dropdown-toggle').dropdown()
 
-'use strict';
-$(function() {
-    //需要拿到传进来的分类 因为在分类标题导航(面包屑导航) 里面需要分类名称
-    //由于地址栏这里获取中文的乱码不能直接把中文 的分类名称传过来 
-    // 所以只能把 分类名称对应的分类id传过来 根据这个分类id去拿到分类名称
-    //根据地址栏的categoryid参数 拿到 categoryid 的值
-    // 为什么要这个值是为了获取这个categoryid 对应的 分类名称
-    var categoryId = getQueryString("categoryid") || 0;
-    //获取地址栏的pageid参数 也就是当前的页码数
-    var pageid = getQueryString("pageid") || 1;
-    getCategory(categoryId);
-    //调用获取商品列表的时候 同时把pageid传入 获取到第几页的数据
-    setProdcutList(categoryId, pageid);
-    //获取商品分类名称函数
-    function getCategory(categoryId) {
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+    var categoryid = getParameter((location.search)).categoryid ||0;
+    var pageid = getParameter((location.search)).pageid ||1;
+     // console.log(pageid);
+
+
+    var category = getParameter(decodeURI(location.search)).category;
+    console.log(category);
+    // 获取商品分类名称函数  比如电视
+    function getCategory(){
         $.ajax({
-            url: "http://193.112.55.79:9090/api/getcategorybyid?categoryid=" + categoryId,
-            success: function(data) {
-                console.log(data.result[0].category);
-                // var html = template("categoryTitleTmp",data);
-                // console.log(html);
-                // $('.breadcrumb').html(html);
-                // $('#productList > .category-title > ol > li:last-child') 获取到分类名称所在的li标签
-                // data.result[0].category 是获取返回的数据的result属性里面的第一个对象里面的category属性
-                $('.breadcrumb > li:last-child').html(data.result[0].category);
+            type: 'get',
+            url: 'http://193.112.55.79:9090/api/getcategorybyid?categoryid=' + categoryid,
+            dataType: 'json',
+            success: function (result) {
+                console.log(result);
+                var html = template('categoryTemp',result)
+                $('.nav').html(html)
             }
-        });
+        }) 
+
     }
-    //获取商品列表的函数
-    function setProdcutList(categoryId, pageid) {
-        // pageid = ;
-        $.ajax({
-            url: "http://193.112.55.79:9090/api/getproductlist",
-            data: {
-                "categoryid": categoryId,
-                "pageid": pageid || 1
-            },
-            success: function(data) {
-                console.log(data);
-                // console.log(data.totalCount +"-----"+ data.pagesize);
-                //页码数 用总条数 /  每页大小
-                var page = data.totalCount / data.pagesize;
-                // console.log(page);
-                var pageli = "";
-                for (var i = 0; i < page; i++) {
-                    //循环生成 第几页的li标签
-                    // var url = "productlist.html?categoryid=" + categoryId + "pageid=" + pageid;
-                    // var url = "productlist.html?categoryid=" + categoryId + "&pageid=" + (i+1);
-                    var url = "productlist.html?categoryid=" + categoryId + "&pageid=" + (i + 1);
-                    pageli += "<li><a href=" + url + ">第" + (i + 1) + "/" + (page) + "页</a></li>";
+    getCategory()
+    function categoryList(categoryid, pageid){
+            $.ajax({
+                url: 'http://193.112.55.79:9090/api/getproductlist',
+                data:{
+                    'categoryid':categoryid,
+                    'pageid':pageid,
+                },
+                dataType: 'json',
+                success: function (result) {
+                    // console.log(result);
+                    result.result.category = category
+                    console.log(result);
+                    console.log({ list: result.result});
+                    var html = template('categoryListTemp',  {list:result.result});
+                    $('.productList').html(html)
+
+
+                     // 修改下拉菜单栏，使得下拉菜单栏的样式也跟随变化。pagenum是总页数
+                    var pagenum = result.totalCount / result.pagesize;
+                    // console.log(pagenum);
+                    var va = $('.dropdown>ul>li>a').text()
+                    console.log(va);
+                    // 循环生成第？页/？页
+                    for (var  i = 0; i < pagenum; i++) {
+                                // 循环生成第几页的li标签
+                        var li = '<li><a href="productlist.html?categoryid=' + categoryid + '&amp;pageid=' + (i+1) + '">第' + (i+1) + '/' + Math.ceil(pagenum) + '页</a></li>'
+                        console.log(li);  //  得到一个标签   
+                       // 并将li追加到ul中
+                        $('.dropdown>ul').append(li)
+                    }
+                    $('#dLabel ').text('第' + pageid +'页')
+
+                    //  设置上一页 下一页的逻辑
+                       /*  if(pageid =1 ){
+                            pageid=2;
+                            pageid++;
+                            $('.page-prev').attr('href','productlist.html?categoryid='+
+                                categoryid + '&amp;pageid=' + (pageid - 1))
+                        }else if(pageid>=pagenum){
+                                pageid = pagenum-1;
+                            $('.page-next').attr('href', 'productlist.html?categoryid=' +
+                                categoryid + '&amp;pageid=' + (parseInt(pageid)+1) )
+                        } */
+
+                    $('.page-prev').on('tap',function(){
+                        pageid--;
+                        $('.page-prev').attr('href', 'productlist.html?categoryid=' +
+                            categoryid + '&amp;pageid=' + pageid)
+                        // 第一页的时候
+                        if (pageid <= 1) {
+                            pageid = 1;
+                            $('.page-prev').attr('href', 'productlist.html?categoryid=' +
+                                categoryid + '&amp;pageid=' + (pageid - 1))
+                        }
+                       
+                    })
+                    $('.page-next').on('tap',function(){
+                            pageid++;
+                        if (pageid >= pagenum) {
+                            pageid = pagenum - 1; 
+                            $('.page-next').attr('href', 'productlist.html?categoryid=' +
+                                categoryid + '&amp;pageid=' + (parseInt(pageid) + 1)) 
+                        }
+                        $('.page-next').attr('href', 'productlist.html?categoryid=' +
+                            categoryid + '&amp;pageid=' + (parseInt(pageid)))
+                    })
+
+
+
+
+
+                    
+                         
+                
+
+
+
+
+
                 }
-                $('#dLabel').html("第" + pageid + "页" + '<span class="caret"></span>');
-                //如果当前页数已经到了第一页 给当前页面数变成2  2 -1 就只能 == 1
-                if (pageid <= 1) {
-                    pageid = 2;
-                } else if (pageid >= page) {
-                    //如果当前页数已经到了第最后一页 给当前页数变成最后一页 - 1  3+1 == 4
-                    pageid = page - 1;
-                }
-                var prevUrl = "productlist.html?categoryid=" + categoryId + "&pageid=" + (pageid - 1);
-                var nextUrl = "productlist.html?categoryid=" + categoryId + "&pageid=" + (parseInt(pageid) + 1);
-                $('.page-prev').attr("href", prevUrl);
-                $('.page-next').attr("href", nextUrl);
-                // $('#dLabel').append('<span class="caret"></span>');
-                $('.dropdown-menu').html(pageli);
-                var html = template("productListTmp", data);
-                $('.product-list').html(html);
-            }
-        })
+
+            })
     }
-    //是用来获取url中的参数的值的 根据参数名获取参数值
-    function getQueryString(name) {
-        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null) {
-            return unescape(r[2]);
+    categoryList(categoryid, pageid)
+        
+
+
+
+
+
+
+
+
+// 获取url地址栏的后面id函数
+    function getParameter(url) {
+        var obj = {}
+        // location.search:url中?及?后面的内容
+        url = url.substring(1) //cid=5&name=jack
+        // 先按&拆分
+        var arr = url.split('&') //['cid=5','name=jack']
+        // 遍历进行第二次拆分
+        for (var i = 0; i < arr.length; i++) {
+            var temp = arr[i].split('=') //['cid',5]
+            obj[temp[0]] = temp[1] // obj['cid'] = 5
         }
-        return null;
+        return obj
     }
-});
+    getParameter(location.search)
+    // decodeURI 当地址栏有中文的时候用
+    // console.log(getParameter(decodeURI(location.search)).category);
+})
